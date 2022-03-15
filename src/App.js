c0a09c81d5d6b405aa03e44a4f4595ebda3becbf
Nesh00 <c.nenad00@gmail.com>
2022-03-15 14:48:35 +0000

    add google fonts, colour variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import './App.css';
+import Nav from './components/Nav';
+import './css/App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='app'>
+      <Nav />
+    </div>
+  );
 }
 
 export default App;
