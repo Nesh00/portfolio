c8c01c7cc59a810a304e8f1855e29c8e27fe30af
Nesh00 <c.nenad00@gmail.com>
2022-03-15 16:47:53 +0000

    add Logo component in Nav

---
+++ src/components/Logo.jsx
@@ -0,0 +1,33 @@
+import '../css/Logo.css';
+import logo from '../images/logo.png';
+
+const Logo = () => {
+  return (
+    <section class='logo--container'>
+      <div id='shape' class='cube backfaces'>
+        <div class='plane one'></div>
+        <div class='plane two'></div>
+        <div class='plane three'></div>
+        <div class='plane four'></div>
+        <div class='plane five'></div>
+        <div class='plane six'></div>
+        <div class='plane seven'></div>
+        <div class='plane eight'>
+          <img src={logo} alt='Logo' />
+        </div>
+        <div class='plane nine'>
+          <img src={logo} alt='Logo' />
+        </div>
+        <div class='plane ten'>
+          <img src={logo} alt='Logo' />
+        </div>
+        <div class='plane eleven'>
+          <img src={logo} alt='Logo' />
+        </div>
+        <div class='plane twelve'></div>
+      </div>
+    </section>
+  );
+};
+
+export default Logo;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,12 @@
 import '../css/Nav/Nav.css';
+import Logo from './Logo';
 
 const Nav = () => {
-  return <nav className='nav'>Nav</nav>;
+  return (
+    <nav className='nav'>
+      <Logo />
+    </nav>
+  );
 };
 
 export default Nav;
