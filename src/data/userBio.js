f677d8b82126bb9587427af13886f3b4640dd87d
Nesh00 <c.nenad00@gmail.com>
2022-03-22 21:21:42 +0000

    create userBio array

---
+++ src/data/userBio.js
@@ -0,0 +1,37 @@
+export const userBio = [
+  {
+    className: 'info',
+    description:
+      'A full-stack developer with a result-driven mindset, time management and quick responsiveness to tasks given.',
+  },
+  { className: 'name', description: 'Nenad Tsvetanovski' },
+  { className: 'location', description: 'Manchester, UK' },
+  {
+    className: 'focus',
+    description:
+      'Main focus is to build scalable and responsive applications that are user friendly and can be easily maintained.',
+  },
+  {
+    className: 'front-end',
+    description: [
+      'HTML',
+      'CSS/SCSS',
+      'JavaScript',
+      'Boostrap',
+      'ReactJS',
+      'React Native/Expo',
+    ],
+  },
+  {
+    className: 'back-end',
+    description: ['NodeJS', 'ExpressJS', 'SQL/PostresQL', 'Firebase 9'],
+  },
+  {
+    className: 'testing',
+    description: ['Jest', 'Supertest'],
+  },
+  {
+    className: 'other',
+    description: ['Trello', 'Agile/SCRUM'],
+  },
+];
