f66f6f74b10b8c60ad79e736bf7e3b5c65f3b081
Nesh00 <c.nenad00@gmail.com>
2022-03-22 21:17:49 +0000

    add cursor color change when hover over Hamburger component

--- src/components/Nav/Hamburger.jsx
+++ src/components/Nav/Hamburger.jsx
@@ -1,14 +1,24 @@
 import '../../css/Nav/Hamburger.css';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useContext } from 'react';
+import { LoaderContext } from '../../context/LoaderContext';
+import { CursorContext } from '../../context/CursorContext';
 
 const Hamburger = ({ setOpenMenu }) => {
+  const { isLoaded } = useContext(LoaderContext);
+  const { cursorChangeHandler } = useContext(CursorContext);
+
   const openMenuHandler = () => {
     setOpenMenu((currState) => !currState);
   };
 
   return (
-    <section className='hamburger__container'>
+    <section
+      className={`hamburger__container ${isLoaded && 'hamburger-red'}`}
+      onMouseEnter={() => cursorChangeHandler('hovered')}
+      onMouseLeave={() => cursorChangeHandler('')}
+    >
       <FontAwesomeIcon icon={faBars} size='3x' onClick={openMenuHandler} />
     </section>
   );
