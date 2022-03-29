d150fc700086ce4e5b0cc50b1aebe5ddfe5b3e65
Nesh00 <c.nenad00@gmail.com>
2022-03-29 16:25:49 +0100

    add Cursor change when hover over Form elements

--- src/components/Contact/Form.jsx
+++ src/components/Contact/Form.jsx
@@ -1,19 +1,46 @@
+import { useContext } from 'react';
+import { CursorContext } from '../../context/CursorContext';
 import '../../css/Contact/Form.css';
 
 const Form = () => {
+  const { cursorChangeHandler } = useContext(CursorContext);
+
   return (
     <form className='contact__form'>
-      <label className='contact__label'>
+      <label
+        className='contact__label'
+        onMouseEnter={() => cursorChangeHandler('cursor--select')}
+        onMouseLeave={() => cursorChangeHandler('')}
+      >
         name <input required type='text' className='contact__input' />
       </label>
-      <label className='contact__label'>
+      <label
+        className='contact__label'
+        onMouseEnter={() => cursorChangeHandler('cursor--select')}
+        onMouseLeave={() => cursorChangeHandler('')}
+      >
         email <input required type='email' className='contact__input' />
       </label>
-      <label className='contact__label'>
+      <label
+        className='contact__label'
+        onMouseEnter={() => cursorChangeHandler('cursor--select')}
+        onMouseLeave={() => cursorChangeHandler('')}
+      >
         tel number <input required type='phone' className='contact__input' />
       </label>
-      <textarea required className='contact__textarea'></textarea>
-      <button className='contact__send'>Send</button>
+      <textarea
+        required
+        className='contact__textarea'
+        onMouseEnter={() => cursorChangeHandler('cursor--select')}
+        onMouseLeave={() => cursorChangeHandler('')}
+      ></textarea>
+      <button
+        className='contact__send'
+        onMouseEnter={() => cursorChangeHandler('cursor--send')}
+        onMouseLeave={() => cursorChangeHandler('')}
+      >
+        Send
+      </button>
     </form>
   );
 };
