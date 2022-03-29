c5c91d7ea15533613e68972aae0082b804d03641
Nesh00 <c.nenad00@gmail.com>
2022-03-30 00:24:36 +0100

    refactor Home header and font size

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,11 +4,11 @@ import Blackhole from './Blackhole';
 const Home = () => {
   return (
     <header className='header'>
-      <h1 className='intro__header'>Hi.</h1>
+      <h1 className='intro__header'>Hi,</h1>
       <h1 className='intro__header'>
         i'm N<span className='intro__header--letter'>e</span>nad,
       </h1>
-      <h1 className='intro__header'>web developer</h1>
+      <h1 className='intro__header'>software developer</h1>
       <h2 className='intro__sub-header'>
         Front End <span className='intro__sub-header--slash'> / </span>Back End
         Developer
