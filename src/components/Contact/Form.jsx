d591f2e73b1fbcb7a218834a8b2660a294df8f02
nenad <c.nenad00@gmail.com>
2022-11-26 00:16:35 +0000

    fix form

--- src/components/Contact/Form.jsx
+++ src/components/Contact/Form.jsx
@@ -1,99 +1,48 @@
-import { useContext, useState } from 'react';
-import { CursorContext } from '../../context/CursorContext';
-import { send } from 'emailjs-com';
+import emailjs from '@emailjs/browser';
 import '../../css/Contact/Form.css';
 import Social from './Social';
+import { useRef } from 'react';
 
 const Form = () => {
-  const { cursorChangeHandler } = useContext(CursorContext);
-  const [toSend, setToSend] = useState({
-    name: '',
-    email: '',
-    number: '',
-    message: '',
-  });
+  const formRef = useRef();
 
-  const changeHandler = (event) => {
-    setToSend({ ...toSend, [event.target.name]: event.target.value });
-  };
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-  const submitFormHandler = (event) => {
-    event.preventDefault();
+    emailjs
+      .sendForm(
+        'service_v3513bq',
+        'template_hxcpase',
+        formRef.current,
+        'grPykzGLOJ5SfLfV5'
+      )
+      .catch((error) => alert(error.text));
 
-    send('service_v3513bq', 'template_hxcpase', toSend, 'grPykzGLOJ5SfLfV5')
-      .then(() => {
-        setToSend({ name: '', email: '', number: '', message: '' });
-      })
-      .catch((err) => {
-        alert('FAILED...', err);
-      });
+    formRef.current.reset();
   };
 
   return (
     <section className='form__section'>
-      <form className='contact__form' onSubmit={submitFormHandler}>
-        <label
-          className='contact__label'
-          onMouseEnter={() => cursorChangeHandler('cursor--select')}
-          onMouseLeave={() => cursorChangeHandler('')}
-        >
+      <form ref={formRef} onSubmit={sendEmail} className='contact__form'>
+        <label className='contact__label'>
           name
-          <input
-            value={toSend.name}
-            name='name'
-            type='text'
-            className='contact__input'
-            onChange={changeHandler}
-            required
-          />
+          <input type='text' name='name' className='contact__input' />
         </label>
-        <label
-          className='contact__label'
-          onMouseEnter={() => cursorChangeHandler('cursor--select')}
-          onMouseLeave={() => cursorChangeHandler('')}
-        >
+        <label className='contact__label'>
           email
-          <input
-            value={toSend.email}
-            name='email'
-            type='email'
-            className='contact__input'
-            onChange={changeHandler}
-            required
-          />
+          <input type='email' name='email' className='contact__input' />
         </label>
-        <label
-          className='contact__label'
-          onMouseEnter={() => cursorChangeHandler('cursor--select')}
-          onMouseLeave={() => cursorChangeHandler('')}
-        >
-          tel number
+        <label className='contact__label'>
+          number
           <input
-            value={toSend.number}
-            name='number'
-            type='text'
+            type='number'
             pattern='07[0-9]{9}'
+            name='number'
             className='contact__input'
-            onChange={changeHandler}
-            required
           />
         </label>
-        <textarea
-          value={toSend.message}
-          name='message'
-          className='contact__textarea'
-          onMouseEnter={() => cursorChangeHandler('cursor--select')}
-          onMouseLeave={() => cursorChangeHandler('')}
-          onChange={changeHandler}
-          required
-        ></textarea>
-        <button
-          className='contact__send'
-          onMouseEnter={() => cursorChangeHandler('cursor--send')}
-          onMouseLeave={() => cursorChangeHandler('')}
-        >
-          Send
-        </button>
+        <textarea name='message' className='contact__textarea' />
+        <input type='submit' value='Send' className='contact__send' />
       </form>
       <Social />
     </section>
